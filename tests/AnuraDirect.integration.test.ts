71c51d72c4bf95f2ff45a4ea240c169e90328711
Xavier Mejia <xmejia@anura.io>
2025-02-13 14:08:38 -0600

    update tests to use latest changes

--- tests/AnuraDirect.integration.test.ts
+++ tests/AnuraDirect.integration.test.ts
@@ -1,13 +1,18 @@
 import { AnuraDirect } from "../src/direct/AnuraDirect";
+import { describe, it, expect } from '@jest/globals';
 
 describe('AnuraDirect client', () => {
   it('throws an error if the instance ID is empty', async() => {
     const direct = new AnuraDirect('');
-    await expect(direct.getResult('127.0.0.1')).rejects.toThrow('Instance not specified');
+    await expect(direct.getResult({
+      ipAddress: '127.0.0.1'
+    })).rejects.toThrow('Instance not specified');
   });
 
   it('throws an error if the instance ID is invalid', async() => {
     const direct = new AnuraDirect('invalid-instance');
-    await expect(direct.getResult('127.0.0.1')).rejects.toThrow('Instance not found');
+    await expect(direct.getResult({
+      ipAddress: '127.0.0.1'
+    })).rejects.toThrow('Instance not found');
   });
 });
