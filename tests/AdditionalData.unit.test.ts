00a3ff6bf4779073c10a4819a78ad99bbcf50e99
Xavier Mejia <xmejia@anura.io>
2025-02-20 14:01:22 -0600

    update AdditionalData tests to reflect latest changes

--- tests/AdditionalData.unit.test.ts
+++ tests/AdditionalData.unit.test.ts
@@ -13,22 +13,22 @@ describe('Additional data', () => {
   })
 
   it('should be able to add a new element', () => {
-    additionalData.addElement('1', 'test-value');
+    additionalData.addElement(1, 'test-value');
     expect(additionalData.size()).toBe(1);
   });
 
   it('should be able to remove additional data', () => {
-    additionalData.addElement('1', 'first-value');
-    additionalData.addElement('2', 'second-value');
-    additionalData.addElement('3', 'third-value');
+    additionalData.addElement(1, 'first-value');
+    additionalData.addElement(2, 'second-value');
+    additionalData.addElement(3, 'third-value');
 
-    additionalData.removeElement('2');
+    additionalData.removeElement(2);
     expect(additionalData.size()).toBe(2);
   });
 
   it('should stringify correctly', () => {
-    additionalData.addElement('1', 'first-value');
-    additionalData.addElement('2', 'second-value');
+    additionalData.addElement(1, 'first-value');
+    additionalData.addElement(2, 'second-value');
 
     expect(additionalData.toString()).toEqual(
       `{"1":"first-value","2":"second-value"}`
