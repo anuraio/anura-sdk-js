72575517c56f9d98cc26248364a0f27c6482652f
Xavier Mejia <xmejia@anura.io>
2025-02-20 14:01:01 -0600

    change AdditionalData to use numbers as keys, rather than strings

--- src/direct/AdditionalData.ts
+++ src/direct/AdditionalData.ts
@@ -2,22 +2,22 @@
  * Additional Data class to represent Additional Data that can be passed to Anura Direct when getting results.
  */
 export class AdditionalData {
-  private _data: Map<string, string> = new Map();
+  private _data: Map<number, string> = new Map();
 
   /**
    * Adds an element (key/value pair) of additional data.
-   * @param {string} key
+   * @param {number} key
    * @param {string} value
    */
-  public addElement(key: string, value: string) {
+  public addElement(key: number, value: string) {
     this._data.set(key, value);
   }
 
   /**
    * Removes an element (key/value pair) from your additional data.
-   * @param {string} key The key of the element you would like to remove.
+   * @param {number} key The key of the element you would like to remove.
    */
-  public removeElement(key: string) {
+  public removeElement(key: number) {
     this._data.delete(key);
   }
 
